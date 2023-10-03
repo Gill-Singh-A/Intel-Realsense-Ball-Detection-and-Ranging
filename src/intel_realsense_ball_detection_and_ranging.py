9c9ab7c077b3569f58cf650cee8a696dabac8ed8
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-04 00:43:01 +0530

    Printing Distance of Ball in mm

--- src/intel_realsense_ball_detection_and_ranging.py
+++ src/intel_realsense_ball_detection_and_ranging.py
@@ -8,12 +8,24 @@ bridge = CvBridge()
 
 sample_space = 5
 tolerence = 10
-score_threshold = 1
+score_threshold = 0
 
 images = []
+center, radius = None, None
 
 def getImage(ros_image):
     images.append(cv2.cvtColor(bridge.imgmsg_to_cv2(ros_image), cv2.COLOR_RGB2BGR))
+def getDepthImage(ros_depth_image):
+    depth_image = bridge.imgmsg_to_cv2(ros_depth_image, ros_depth_image.encoding)
+    if center == None and radius == None:
+        return
+    depths = []
+    for x in range(center[0]-radius, center[0]+radius):
+        for y in range(center[1]-round(math.sqrt(radius**2-(center[0]-x)**2)), center[1]+round(math.sqrt(radius**2-(center[0]-x)**2))-1):
+            depths.append(depth_image[y][x])
+    total_depth = sum(depths)
+    total_depths = len(depths)
+    print(total_depth/total_depths)
 
 def dist(c_1, c_2):
     return math.sqrt((c_1[0]-c_2[0])**2+(c_1[1]-c_2[1])**2)
@@ -65,12 +77,16 @@ if __name__ == "__main__":
     rospy.init_node("intel_realsense_ball_detection_and_ranging")
     rate = rospy.Rate(10)
     image_subscriber = rospy.Subscriber("/camera/color/image_raw", Image, getImage)
+    image_depth_subscriber = rospy.Subscriber("/camera/depth/image_rect_raw", Image, getDepthImage)
     while not rospy.is_shutdown():
         if len(images) == sample_space:
             ball = detectBalls(images)
             if ball != None:
-                cv2.circle(images[0], (ball[0], ball[1]), ball[2], (0, 255, 0), 2)
+                center, radius = (ball[0], ball[1]), ball[2]
+                cv2.circle(images[0], center, radius, (0, 255, 0), 2)
                 cv2.rectangle(images[0], (ball[0]-1, ball[1]-1), (ball[0]+1, ball[1]+1), (255, 0, 0), -1)
+            else:
+                center, radius = None, None
             cv2.imshow("Ball Detection", images[0])
             cv2.waitKey(1)
             images.clear()
